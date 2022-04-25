1458da4bfbfa8d5bf9b5e34ea7fecef3d40dafe2
Ade Yusuf <adeyusuf005@gmail.com>
2022-04-25 10:24:25 +0700

    Redesign dengan menggunakan tailwindcss

--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 # MY PORTFOLIO WEBSITE
 
-Ini adalah website portfolio pribadi saya yang menyajikan daftar karya - karya yang telah saya buat sebelumnya. DImana website ini dibuat dengan menggunakan teknologi `html`, `css` dan `javascript`
+Ini adalah website portfolio pribadi saya yang menyajikan daftar karya - karya yang telah saya buat sebelumnya. DImana website ini dibuat dengan menggunakan teknologi `html`, `css`, `javascript` dan `tailwindcss`.
---
+++ src/js/script.js
@@ -0,0 +1,29 @@
+// Script for auto updating a year in footer
+// const getTahunCopyright = (copyright) => {
+//     const year = new Date().getFullYear();
+//     return `${copyright} &copy; ${year}`;
+// }
+
+// const footerCopy = document.querySelector('.footer-copy h4');
+// const content    = `<a href="https://www.instagram.com/adeyusuf.official" target="_blank">Ade Yusuf</a>`;
+
+// footerCopy.innerHTML = getTahunCopyright(content);
+// End auto updating year in footer script
+
+// script for smooth scrolling
+const links = document.querySelectorAll("#nav ul li a");
+
+links.forEach(link => {
+    link.addEventListener('click', clickHandler);
+});
+
+function clickHandler(e) {
+  e.preventDefault();
+  const href        = this.getAttribute("href");
+  const offsetTop   = document.querySelector(href).offsetTop;
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}
+// end smooth scrolling script
